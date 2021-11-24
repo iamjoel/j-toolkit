d7a9fa9804217cb35d2ec4a628112f428e4b0a91
金伟强 <jinweiqiang@coding.net>
2021-11-24 17:35:08 +0800

    add frontend

---
+++ frontend/README.md
@@ -0,0 +1,5 @@
+# Joel 工具箱 前端
+Vue3 + TypeScript。
+
+## 工具
+- [Volar](https://marketplace.visualstudio.com/items?itemName=johnsoncodehk.volar) VSCode 插件。
---
+++ frontend/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+</template>
+
+<style>
+
+</style>
